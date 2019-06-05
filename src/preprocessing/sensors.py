2fc1cca245a4424555127b981065778f3ad32d47
giovanni <giovanni.gabbolini@gmail.com>
2019-06-05 23:38:43 +0200

    path fixed in preprocessed sensors

--- src/preprocessing/sensors.py
+++ src/preprocessing/sensors.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-
+from src import data
 import numpy as np
 import pandas as pd
 import src.data as data
@@ -9,15 +9,14 @@ from src.utility import check_folder
 
 def preprocess():
     print('Preprocessing sensors...')
-    _BASE_PATH = 'resources/dataset/preprocessed'
-    check_folder(_BASE_PATH)
 
     # drop the duplicates rows
     sensors_df = data.sensors_original()
     print('dropping duplicates...')
     sensors_df = sensors_df.drop_duplicates()
-    print(f'saving to {_BASE_PATH}/sensors.csv.gz')
-    sensors_df.to_csv(path_or_buf=f'{_BASE_PATH}/sensors.csv.gz', index=False, compression='gzip')
+    
+    path = data.get_path_preprocessed('', '', 'sensors.csv.gz')
+    sensors_df.to_csv(path, index=False, compression='gzip')
 
 if __name__ == '__main__':
     preprocess()
